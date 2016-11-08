b910621f8942f451dd8bb6911a92f4a6dcfc74a7
diglungdig <tachyon.of.tear@gmail.com>
2016-11-08 02:07:47 -0500

    commit 1

---
+++ PVRC-SurvivalGame/Assets/Scripts/Grab.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grab : MonoBehaviour {
+
+    // Hand (low poly) by anura is licensed under CC Attribution
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
